61bed20844e2d1e3c42a3c02d828490b96c34cf1
Isabela Oliveira <oliver.isabela2303@gmail.com>
2023-03-31 16:37:34 -0300

    saving progress #19

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,20 +4,33 @@ import BlogList from './BlogList';
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setisPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
-      .then(r => r.json())
+      .then(r => {
+        if (!r.ok) {
+          throw Error('Could not fetch the data for that resource.');
+        } else {
+          return r.json();
+        }
+      })
       .then(data => {
         setBlogs(data);
+        setisPending(false);
+        setError(null);
+      })
+      .catch(err => {
+        setError(err.message);
         setisPending(false);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{ error }</div>}
       {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs}/>}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 };
