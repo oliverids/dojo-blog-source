d610b8454fd394c22801a0019fcbe42728c7c6c5
Isabela Oliveira <oliver.isabela2303@gmail.com>
2023-03-30 22:01:28 -0300

    saving progress

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,25 @@
+import { useState } from 'react';
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
 
-  const HandleClick = () => {
-    console.log('Button was clicked');
-  };
-
-  return ( 
+  return (
     <div className="home">
-      <h1>Homepage</h1>
-
-      <button onClick={HandleClick}>Click me</button>
+      <ul>
+        {blogs.map(blog => (
+          <li key={blog.id} className='blog-preview'>
+            <h2>{blog.title}</h2>
+            <p>{blog.author}</p>
+            {/* <p>{blog.body}</p> */}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
- 
+
 export default Home;
